96dea994a4093085c787d392bbcebe45897a503e
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-02-13 12:24:33 +0000

    first commit

---
+++ src/components/BarChart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BarChart() {
+  return (
+    <div className="BarChart">
+      <hr/>
+ <h2>BarChar</h2>
+ <hr/>
+    </div>
+  );
+}
+
+export default BarChart;
