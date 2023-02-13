13b717789a1036e756edbe361907f80687215982
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-02-13 17:06:46 +0000

    Tried barchart now trying other charts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import BarChart from './components/BarChart';
 
 function App() {
   return (
     <div className="App">
  <h1>Charts JS App</h1>
+ <BarChart />
     </div>
   );
 }
